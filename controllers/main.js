15c19277fc32623a75b9c417c0e7480f0a159349
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-10 09:31:08 +0530

    added jwt token

---
+++ controllers/main.js
@@ -0,0 +1,29 @@
+const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken')
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    console.log(username, password);
+
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email password', 400);
+    }
+
+    const id = new Date().getDate();
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    res.status(200).json({ msg: 'User Created', token });
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `Hello, Shreesha H R`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+};
+
+module.exports = { login, dashboard };
